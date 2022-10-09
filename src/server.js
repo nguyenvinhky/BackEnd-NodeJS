96e550a91ef61637aadc60115dbb2ed9c3487c80
vinhkynguyen <info.vinhky@gmail.com>
2022-10-09 22:05:35 +0700

    Configuration server web fast food equal nodejs

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import viewEngine from "./config/viewEngine";
+import initWebRouters from "./route/web";
+require('dotenv').config();
+
+
+let app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+viewEngine(app);
+initWebRouters(app);
+
+let port = process.env.PORT || 6969;
+app.listen(port, () =>{
+    console.log(`Back-end NodeJS is running on the port : ${port}`);
+});
