af4fb278995f4b55108644fdd6ba84c314b59d9e
vinhkynguyen <info.vinhky@gmail.com>
2022-10-13 12:45:02 +0700

    connect to database with mysql demo

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -1,6 +1,17 @@
 
-let homeControllers = (req, res) => {
-    return res.render('demo.ejs');
+
+import db from '../models/index';
+let homeControllers = async (req, res) => {
+    try {
+        let data = await db.User.findAll();
+        
+        return res.render('demo.ejs', {
+            data: JSON.stringify(data)
+        });
+    } catch (e) {
+        console.log(e);
+    }
+    
 }
 
 module.exports = {
